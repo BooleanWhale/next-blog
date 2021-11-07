8dd5693e5143d6f602c4e2e8c61a0e71319dac53
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2021-11-07 16:40:03 +0000

    about me text

--- pages/components/homePage/AboutMeSection.js
+++ pages/components/homePage/AboutMeSection.js
@@ -8,10 +8,15 @@ export default function AboutMeSection() {
         <div id="about-me-info-cont">
             <div id="about-me-info">
                 <h2>About Me</h2>
-                <p>Hi, my name is Ashley Saleem-West. I'm a web developer based St Albans, a wonderful town near London in the UK. I describe myself as a passionately user-focused developer who loves coding, design, and the web platform.</p>
-                <p>Starting as a designer and becoming acquainted with web development through personal E-Commerce projects before moving onto E-Service Europe Ltd, to redesign and develop their E-Commerce website.</p>
-                <p> With experiece in MySQL databases and React.JS, I am always pushing myself to learn more and have been making use of the time saved from commuting by participating in Harvard's CS50 course for Web Programming.</p>
-                <p>I think of performance and design as two sides of the same coin, which is why I push for great user experience without a heavy overhead. I like to ask myself how I can make an application easy, enjoyable,and fast to use.</p>
+                <p>
+                Hi, my name is Ashley Saleem-West. I'm a web developer with a background in design, based in St Albans (a wonderful town near London in the UK). I currently work at Underwaterpistol as a member of the web development team. I work with SCSS, HTML, JS and Liquid to develop bespoke Shopify Plus stores.
+                </p>
+                <p>
+                Starting as a designer and becoming acquainted with web development through personal E-Commerce projects, I moved onto E-Service Europe Ltd to redesign and develop their E-Commerce website.
+                </p>
+                <p>
+                With experience in SQL databases and React.JS, I am always pushing myself to learn more in my spare time. I’ve worked on personal projects to become familiar with Libraries and frameworks that personally interest me, including Next.js and Tailwind CSS. I also like to keep up with the best practices for CSS and achieve greater performance and security by relying on pure CSS for user interactions and animations.
+                </p>
                 <p>When I'm not coding or designing, I can be found drawing or caring for my <a href="https://www.instagram.com/jaqenclownfish/">two adorable clownfish.</a></p>
             </div>
         </div>
